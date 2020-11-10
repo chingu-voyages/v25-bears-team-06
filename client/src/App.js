47ce6d60507526681aeefe952f6870b38cd3510c
Michelle <16948138+michellecan@users.noreply.github.com>
2020-11-09 20:36:22 -0800

    client folder with material, airbnb lint packages, router, moment, root folder with nodemon and concurrently

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <h1>welcome to book share</h1>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
