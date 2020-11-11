dc4837d2cebfe94921dcea06d30ec5b09b2288d3
Babak Chehraz <babak.chehraz@gmail.com>
2020-11-10 17:42:58 -0800

    [ESLint Rules Added] Formatted files with prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <h1>welcome to book share</h1>
-    </div>
+      <div className="App">
+        <h1>welcome to book share</h1>
+      </div>
     </Router>
   );
 }
