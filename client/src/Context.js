5589bee46ec86e2e0e60228af4303207ef438d14
silentkdev <andrewknox@silentk.dev>
2020-11-25 11:08:39 -0800

    adding authorization context

--- client/src/Context.js
+++ client/src/Context.js
@@ -2,3 +2,5 @@ import { createContext } from "react";
 
 // eslint-disable-next-line import/prefer-default-export
 export const SearchContext = createContext(null);
+
+export const AuthContext = createContext(null);
