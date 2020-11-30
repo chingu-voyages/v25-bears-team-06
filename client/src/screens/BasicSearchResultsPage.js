17532c4dc6d32cbddd4a256786310ef4349f6a3d
silentkdev <andrewknox@silentk.dev>
2020-11-29 16:44:35 -0800

    refactoring basic search page to handle new network request

--- client/src/screens/BasicSearchResultsPage.js
+++ client/src/screens/BasicSearchResultsPage.js
@@ -1,42 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import dummyData from "../dummyData";
 import SingleBookCard from "../components/SingleBookCard";
 import { SearchContext } from "../Context";
+import searchRequest from "../dataservice/searchRequest";
+
+const useStyles = makeStyles({
+  errorDiv: {
+    height: "50%",
+  },
+});
 
 // Display Search Results in a Grid
 
 const BasicSearchResultsPage = () => {
-  const { value, setValue } = useContext(SearchContext);
+  const [bookResults, setBookResults] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(
+    "Something went wrong. Please try again later.",
+  );
+  const { query } = useContext(SearchContext);
 
-  const searchTerm = value;
-  // setValue is updated in basicSearchBox.js
-  // eslint-disable-next-line no-unused-expressions
-  setValue;
+  useEffect(function handleEffect() {
+    async function searchEffect() {
+      try {
+        const { books, message } = await searchRequest({ query });
+        if (books && Array.isArray(books)) {
+          setBookResults(books);
+        } else if (message) {
+          setHasError(true);
+          setErrorMessage(message);
+        }
+      } catch (err) {
+        setHasError(true);
+        setErrorMessage(err.message);
+      }
+    }
+    searchEffect().catch((err) => {
+      setHasError(true);
+      setErrorMessage(err.message);
+    });
+  }, []);
+
+  const classes = useStyles();
 
   return (
     <div>
+      {hasError && <div className={classes.errorDiv}>{errorMessage}</div>}
       <h1>Search Results Page</h1>
       <Typography variant="subtitle1">
         Authors and titles that contain:{" "}
-        <span style={{ color: "red" }}>{value}</span>
+        <span style={{ color: "red" }}>{query}</span>
       </Typography>
 
-      {dummyData
+      {bookResults
         .filter(
           (book) =>
-            book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            book.author.toLowerCase().includes(searchTerm.toLowerCase()),
+            book.title.toLowerCase().includes(query.toLowerCase()) ||
+            book.author.toLowerCase().includes(query.toLowerCase()),
         )
         .map((filteredBook) => (
-          <div>
-            <SingleBookCard
-              key={filteredBook.id}
-              title={filteredBook.title}
-              author={filteredBook.author}
-              thumbnail={filteredBook.thumbnail}
-            />
-          </div>
+          <SingleBookCard
+            key={filteredBook._id}
+            title={filteredBook.title}
+            author={filteredBook.author}
+            thumbnail={filteredBook.thumbnail}
+          />
         ))}
     </div>
   );
