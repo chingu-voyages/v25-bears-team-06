84913d9343b94628d314c447b5ccc8046c4b9125
silentkdev <andrewknox@silentk.dev>
2020-12-02 23:36:59 -0800

    making the linter happy

--- client/src/components/CheckOutModal.js
+++ client/src/components/CheckOutModal.js
@@ -5,7 +5,6 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import Dialog from "@material-ui/core/Dialog";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
