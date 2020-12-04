55224a0273cfcea49fd81f484af05efd5328b57e
silentkdev <andrewknox@silentk.dev>
2020-12-03 23:43:16 -0800

    refactoring base request to account for graphql sending errors irrespective of status codes and response.ok

--- client/src/dataservice/baseRequest.js
+++ client/src/dataservice/baseRequest.js
@@ -2,12 +2,12 @@ export default async function baseRequest(opts) {
   const url = process.env.REACT_APP_API_URL || "http://localhost:5000/graphql";
   const response = await fetch(url, opts);
   const result = await response.json();
-  if (response.ok) {
-    const { data } = result;
-    return data;
+  if (result.errors) {
+    const {
+      errors: [message],
+    } = result;
+    return message;
   }
-  const {
-    errors: [message],
-  } = result;
-  return message;
+  const { data } = result;
+  return data;
 }
