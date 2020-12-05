1ec9d8f7c7508ee87c535c485973f416fc6d67e2
Babak Chehraz <babak.chehraz@gmail.com>
2020-12-05 02:05:57 -0800

    Adds useQuery hook with searchBooks file containing query+variables

---
+++ client/src/dataservice/useQuery.js
@@ -0,0 +1,76 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const useQuery = ({ query, variables, token }) => {
+  const history = useHistory();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const doQuery = async () => {
+      setLoading(true);
+
+      const reqBody = {
+        query,
+        variables,
+      };
+
+      const url =
+        process.env.REACT_APP_API_URL || "http://localhost:5000/graphql";
+
+      const authorization = token
+        ? {
+            Authorization: `Bearer ${token}`,
+          }
+        : null;
+
+      const opts = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization,
+        },
+        body: JSON.stringify(reqBody),
+      };
+
+      try {
+        const response = await fetch(url, opts);
+
+        if (!response.ok) {
+          const { status } = response;
+
+          history.replace(history.location.pathname, {
+            errorStatusCode: status,
+          });
+        }
+
+        const result = await response.json();
+
+        if (result.errors) {
+          const {
+            errors: [message],
+          } = result;
+          setError(message);
+        } else if (result.data) {
+          setData(result.data);
+        }
+
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        setError(`Something went wrong. Please try again late ${err}`);
+      }
+    };
+    doQuery();
+  }, [JSON.stringify(variables), token]);
+
+  return {
+    loading,
+    data,
+    error,
+  };
+};
+
+export default useQuery;
