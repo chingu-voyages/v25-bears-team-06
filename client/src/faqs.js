7c7935491a8b88950e7ef010c6047b9e534cf570
Michelle <16948138+michellecan@users.noreply.github.com>
2020-12-06 12:21:50 -0800

    add faqs template to about page

---
+++ client/src/faqs.js
@@ -0,0 +1,19 @@
+const FAQs = [
+  {
+    id: "q1",
+    questionText: "First question here",
+    details: "first answer here",
+  },
+  {
+    id: "q2",
+    questionText: "Second question here",
+    details: "Second answer here",
+  },
+  {
+    id: "q3",
+    questionText: "Third question here",
+    details: "Third answer here",
+  },
+];
+
+export default FAQs;
