3e68579625e00a785dc282a189d69cb7107c9f38
silentkdev <andrewknox@silentk.dev>
2020-12-07 15:06:33 -0800

    inventory request

---
+++ client/src/dataservice/getInventoryRequest.js
@@ -0,0 +1,45 @@
+import baseRequest from "./baseRequest";
+
+export default async function getInventoryRequest({ token }) {
+  const reqBody = {
+    query: `
+      query Inventory {
+        getInventory {
+          _id
+          book {
+            _id
+            googleId
+            title
+            authors
+            description
+            categories
+            pageCount
+            publishedDate
+            publisher
+          }
+          checkoutData {
+            user {
+              displayName
+            }
+            checkoutDate
+            dueDate
+            returnDate
+            condition
+          }
+          isAvailable
+        }
+      }
+    `,
+  };
+
+  const opts = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(reqBody),
+  };
+
+  return baseRequest(opts);
+}
