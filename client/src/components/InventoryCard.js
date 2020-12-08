b655f30028d4d9df7feb861aa5ea1c3cb8ddb345
silentkdev <andrewknox@silentk.dev>
2020-12-08 11:45:55 -0800

    initial accordion implementation for inventory card

--- client/src/components/InventoryCard.js
+++ client/src/components/InventoryCard.js
@@ -1,12 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/styles";
 import Button from "@material-ui/core/Button";
-import { Paper, Typography } from "@material-ui/core";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { format } from "date-fns";
-import { AuthContext } from "../Context";
-import { RETURN_BOOK, REMOVE_BOOK } from "../dataservice/mutations";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import CancelIcon from "@material-ui/icons/Cancel";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import useMutation from "../dataservice/useMutation";
+import { RETURN_BOOK, REMOVE_BOOK } from "../dataservice/mutations";
+import { AuthContext } from "../Context";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,6 +41,12 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
   },
   inventoryItemStatusAvailable: {
+    width: "100%",
+  },
+  inventoryStatusAccordion: {
+    width: "100%",
+  },
+  inventoryStatusAccordionSummary: {
     display: "flex",
     flexFlow: "row nowrap",
     justifyContent: "space-between",
@@ -67,11 +83,22 @@ export default function InventoryCard({
   isAvailable,
 }) {
   const auth = useContext(AuthContext);
+  const [expanded, setExpanded] = useState(false);
 
   const [
     returnBook,
-    // { data: returnData, error: returnError, loading: returnLoading },
+    { data: returnData, error: returnError, loading: returnLoading },
   ] = useMutation(RETURN_BOOK.mutation, auth.user.token);
+
+  useEffect(() => {
+    if (returnData) {
+      console.log(returnData);
+    } else if (returnError) {
+      console.log(returnError);
+    } else {
+      console.log("wtf");
+    }
+  }, [returnData, returnError]);
 
   const handleReturn = async () => {
     await returnBook(
@@ -90,6 +117,10 @@ export default function InventoryCard({
 
   const handleRemove = async () => {
     await removeBook(REMOVE_BOOK.variables({ ownershipId: id }));
+  };
+
+  const toggleExpanded = (currentAccordion) => (event, isExpanded) => {
+    setExpanded(isExpanded ? currentAccordion : false);
   };
 
   const classes = useStyles();
@@ -116,21 +147,53 @@ export default function InventoryCard({
       <div>
         {isAvailable ? (
           <div className={classes.inventoryItemStatusAvailable}>
-            <Typography
-              className={classes.inventoryItemStatusText}
-              variant="body1"
-              component="p"
+            <Accordion
+              className={classes.inventoryStatusAccordion}
+              expanded={expanded === id}
+              onChange={toggleExpanded(id)}
             >
-              Available
-            </Typography>
-            <Button
-              onClick={handleRemove}
-              className={classes.inventoryItemStatusBtn}
-              variant="contained"
-              startIcon={<DeleteIcon />}
-            >
-              Remove
-            </Button>
+              <AccordionSummary
+                classes={{ content: classes.inventoryStatusAccordionSummary }}
+              >
+                <Typography
+                  className={classes.inventoryItemStatusText}
+                  variant="body1"
+                  component="p"
+                >
+                  Available
+                </Typography>
+                <Button
+                  className={classes.inventoryItemStatusBtn}
+                  variant="contained"
+                  startIcon={<DeleteIcon />}
+                  onClick={toggleExpanded(id)}
+                >
+                  Remove
+                </Button>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Grid item container xs={12}>
+                  <Grid item xs={9}>
+                    <Typography variant="h6">Confirm Removal</Typography>
+                  </Grid>
+
+                  <Grid item container xs={3}>
+                    <IconButton
+                      className={classes.cancel}
+                      onClick={toggleExpanded(id)}
+                    >
+                      <CancelIcon className={classes.iconBtn} />
+                    </IconButton>
+                    <IconButton
+                      className={classes.confirm}
+                      onClick={handleRemove}
+                    >
+                      <CheckCircleIcon className={classes.iconBtn} />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </AccordionDetails>
+            </Accordion>
           </div>
         ) : (
           <div>
