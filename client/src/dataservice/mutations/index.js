4a90621aa61f70969906c504d64ec0cb23fbe92d
silentkdev <andrewknox@silentk.dev>
2020-12-07 23:58:27 -0800

    adding return book mutation to index mutation file

--- client/src/dataservice/mutations/index.js
+++ client/src/dataservice/mutations/index.js
@@ -1,5 +1,6 @@
 import LOGIN from "./login";
 import SIGNUP from "./signup";
 import UPLOAD_BOOK from "./uploadBook";
+import RETURN_BOOK from "./returnBook";
 
-export { LOGIN, SIGNUP, UPLOAD_BOOK };
+export { LOGIN, SIGNUP, UPLOAD_BOOK, RETURN_BOOK };
