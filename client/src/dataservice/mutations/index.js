8ed8bc9a961f8eabb2acd5c45e84576929eac2ae
Babak Chehraz <babak.chehraz@gmail.com>
2020-12-07 16:33:28 -0800

    Moves more queries/mutations into new format

--- client/src/dataservice/mutations/index.js
+++ client/src/dataservice/mutations/index.js
@@ -1,5 +1,6 @@
 import LOGIN from "./login";
 import SIGNUP from "./signup";
 import UPLOAD_BOOK from "./uploadBook";
+import CHECKOUT_BOOK from "./checkoutBook";
 
-export { LOGIN, SIGNUP, UPLOAD_BOOK };
+export { LOGIN, SIGNUP, UPLOAD_BOOK, CHECKOUT_BOOK };
---
+++ client/src/dataservice/queries/index.js
@@ -0,0 +1,4 @@
+import SEARCH_BOOKS from "./searchBooks";
+import GET_BOOK_BY_ID from "./getBookById";
+
+export { SEARCH_BOOKS, GET_BOOK_BY_ID };
