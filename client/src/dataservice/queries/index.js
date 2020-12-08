d282d3a094676231e84db522fe1541504fec37fd
Babak Chehraz <babak.chehraz@gmail.com>
2020-12-07 22:43:52 -0800

    Adds getInventory query file

--- client/src/dataservice/queries/index.js
+++ client/src/dataservice/queries/index.js
@@ -1,4 +1,5 @@
 import SEARCH_BOOKS from "./searchBooks";
 import GET_BOOK_BY_ID from "./getBookById";
+import GET_INVENTORY from "./getInventory";
 
-export { SEARCH_BOOKS, GET_BOOK_BY_ID };
+export { SEARCH_BOOKS, GET_BOOK_BY_ID, GET_INVENTORY };
