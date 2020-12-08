6c061778c78103f6f5d65665c4529f16d4cb60c8
silentkdev <andrewknox@silentk.dev>
2020-12-08 00:17:35 -0800

    adding remove book mutation to index mutation

--- client/src/dataservice/mutations/index.js
+++ client/src/dataservice/mutations/index.js
@@ -2,5 +2,6 @@ import LOGIN from "./login";
 import SIGNUP from "./signup";
 import UPLOAD_BOOK from "./uploadBook";
 import RETURN_BOOK from "./returnBook";
+import REMOVE_BOOK from "./removeBook";
 
-export { LOGIN, SIGNUP, UPLOAD_BOOK, RETURN_BOOK };
+export { LOGIN, SIGNUP, UPLOAD_BOOK, RETURN_BOOK, REMOVE_BOOK };
